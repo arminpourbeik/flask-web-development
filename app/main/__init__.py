2f8e6fe3fcfe1776e9d39c0af1ed1b41673ec81b
Armin <armin.pourbeik@gmail.com>
2020-09-12 19:14:37 +0430

    templates and run basic app

---
+++ app/main/__init__.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+
+
+main = Blueprint(name='main', import_name=__name__)
+
+from . import views, errors
--- config.py
+++ config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'P@ASSW0RD'
-    SQL_ALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
