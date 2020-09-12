97be845b8c0a4e55b1b543b2fa3338e7292d3a5b
Armin <armin.pourbeik@gmail.com>
2020-09-12 20:02:21 +0430

    add named argument

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
 
-main = Blueprint(name='main', import_name=__name__)
+main = Blueprint(name='main', import_name=__name__, url_prefix='/')
 
 from . import views, errors
