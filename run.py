2d27d40c5e240b16259fc9aaa89c8db1d0ee9bdf
Armin <armin.pourbeik@gmail.com>
2020-09-13 00:18:03 +0430

    added the test cli command

--- run.py
+++ run.py
@@ -9,6 +9,17 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app=app, db=db)
 
 
+@app.cli.command()
+def test(test_names):
+    """Run the unit tests."""
+    import unittest
+    if test_names:
+        tests = unittest.TestLoader().loadTestsFromNames('test_names')
+    else:
+        tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User)
