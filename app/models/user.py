034d0a86922c8ccc917280b5b387b0f57893710d
Armin <armin.pourbeik@gmail.com>
2020-09-15 01:12:31 +0430

    admin-level edit profile

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,9 @@
 from datetime import datetime
+import hashlib
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import JSONWebSignatureSerializer as Serializer
 
-from flask import current_app
+from flask import current_app, request
 
 from flask_login import UserMixin, AnonymousUserMixin
 
@@ -74,6 +75,11 @@ class User(UserMixin, db.Model):
 
     def is_administrator(self):
         return self.can(Permission.ADMIN)
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        url = 'https://secure.gravatar.com/avatar'
+        hash_ = hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'{url}/{hash_}?s={size}&d={default}&r={rating}'
 
     def __repr__(self):
         return f'<User {self.id} - username: {self.username} - role: {self.role}'
