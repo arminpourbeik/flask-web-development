930f8944ae2c973692e36639e7a35e2b34444be0
Armin <armin.pourbeik@gmail.com>
2020-09-18 21:12:11 +0430

    Complete API Implementation.

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 import click
 
@@ -13,21 +14,47 @@ from app.models.post import Post
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app=app, db=db)
 
+
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role, Follow=Follow, Post=Post)
 
 
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
+
 @app.cli.command()
+@click.option('--coverage/--no-coverage', default=False,
+              help='Run tests under code coverage.')
 @click.argument('test_names', nargs=-1)
-def test(test_names):
+def test(coverage, test_names):
     """Run the unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import subprocess
+        os.environ['FLASK_COVERAGE'] = '1'
+        sys.exit(subprocess.call(sys.argv))
+
     import unittest
     if test_names:
-        tests = unittest.TestLoader().loadTestsFromNames('test_names')
+        tests = unittest.TestLoader().loadTestsFromNames(test_names)
     else:
         tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
+
 
 
 # flask db stamp head -> flask db migrate -> flask db upgrade
